08f507e0f858ef4ee1bc492f0d5a86a08624f8cb
Graceagt <graceuu98@gmail.com>
2024-11-04 09:10:55 +0700

    mingyu

---
+++ resources/views/landingpage.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>StudyMate</title>
+  <link rel="stylesheet" href='css/landingpage.css'>
+  <link href="https://fonts.googleapis.com/css2?family=Modak&display=swap" rel="stylesheet">
+
+</head>
+<body>
+  <div class="container">
+    <div class="gradient-section-one">
+      <img src='images/logostudymate.png' alt="StudyMate Logo" class="logo">
+      <span class="logo-text">StudyMate</span>
+    </div>
+  </div>
+</body>
+</html>
