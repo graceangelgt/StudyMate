82da4541f550372dcc18d76cb8ea5aecbb7f8638
almaacp <almaalyaciptaputri@gmail.com>
2024-11-04 22:21:08 +0700

    routing landing page, sign in, sign up

--- resources/views/regist.blade.php
+++ resources/views/regist.blade.php
@@ -15,7 +15,8 @@
         <div class="right-panel">
             <a href="/" class="back-link">← Back</a>
             <h2>Sign Up</h2>
-            <form method="POST" action="signup_process.php"> <!-- Update action URL as needed -->
+            <form method="POST" action="/">
+                @csrf
                 <label for="name">Full Name*</label>
                 <input type="text" id="name" name="name" placeholder="Enter full name" required>
 
@@ -39,6 +40,6 @@
             <p class="signin-link">Already have an account? <a href="/signin">Sign In</a></p>
         </div>
     </div>
-    <script src="login.js"></script>
+    <script src="js/login.js"></script>
 </body>
 </html>
