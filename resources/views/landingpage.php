aeaf400aaf44af03178de87f5e0407a5e65f0759
Graceagt <graceuu98@gmail.com>
2024-11-04 13:00:56 +0700

    eric

--- resources/views/landingpage.php
+++ resources/views/landingpage.php
@@ -4,15 +4,27 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>StudyMate</title>
-  <link rel="stylesheet" href='css/landingpage.css'>
-  <link href="https://fonts.googleapis.com/css2?family=Modak&display=swap" rel="stylesheet">
-
+  <link rel="stylesheet" href="css/landingpage.css">
+  <link href="https://fonts.googleapis.com/css2?family=Chewy&family=Modak&family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Chewy&family=Modak&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
 <body>
   <div class="container">
     <div class="gradient-section-one">
-      <img src='images/logostudymate.png' alt="StudyMate Logo" class="logo">
+      <img src="images/logostudymate.png" alt="StudyMate Logo" class="logo">
       <span class="logo-text">StudyMate</span>
+      <nav class="nav-links">
+        <a href="#about" class="nav-link">About</a>
+        <a href="#features" class="nav-link">Features</a>
+        <!-- Kolom Pencarian -->
+        <div class="search-bar">
+        <i class="fas fa-search icon"></i>
+        <input type="text" placeholder="Search..">
+      </nav>
+</div>
+
+      </nav>
     </div>
   </div>
 </body>
